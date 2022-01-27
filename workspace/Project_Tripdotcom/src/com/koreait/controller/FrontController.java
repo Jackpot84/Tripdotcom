2eb72629879600c2872f0d29479598c845d18313
ohchocho <choroc712@gmail.com>
2022-01-27 16:13:49 +0900

    Update FrontController.java

--- workspace/Project_Tripdotcom/src/com/koreait/controller/FrontController.java
+++ workspace/Project_Tripdotcom/src/com/koreait/controller/FrontController.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.koreait.action.ActionForward;
+
+import com.koreait.user.action.MoveResPage;
+
+import com.koreait.manager.action.ManagerLoginAction;
+
 import com.koreait.user.action.UserJoinAction;
 import com.koreait.user.action.UserLoginAction;
 
@@ -40,17 +45,58 @@ public class FrontController extends HttpServlet{
 				forward = new UserLoginAction().execute(req, resp);
 				break;
 			case "/goUserLogin.do":
-				//db안들림
+				//로그인화면으로 이동
+				forward = new ActionForward();
 				forward.setRedirect(false);
-				forward.setPath("/app/admin/user/login_view.jsp");
+				forward.setPath(req.getContextPath()+"/app/admin/user/login_view.jsp");
 				break;
-			case "/aaaa.do":
-				//db안들림
+			case "/goUserJoin.do":
+				//로그인화면으로 이동
+				forward = new ActionForward();
 				forward.setRedirect(false);
-				forward.setPath("/app/admin/user/login_view.jsp");
+				forward.setPath(req.getContextPath()+"/app/admin/user/joinUser_view.jsp");
 				break;
+			case "/goReservationAll.do":
+				//내 예약 페이지로 이동
+				forward = new ActionForward();
+				forward.setRedirect(false);
+				forward.setPath(req.getContextPath()+"/app/admin/user/reservationAll.jsp");
+				break;
+			case "/goMyFavorite.do":
+				//즐겨찾기 페이지로 이동
+				forward = new ActionForward();
+				forward.setRedirect(false);
+				forward.setPath(req.getContextPath()+"/app/admin/user/myFavorite.jsp");
+				break;
+			case "/goMyTripcoin.do":
+				//트립코인 페이지로 이동
+				forward = new ActionForward();
+				forward.setRedirect(false);
+				forward.setPath(req.getContextPath()+"/app/admin/user/myTripcoin.jsp");
+				break;
+			case "/goMyInformation.do":
+				//내정보 페이지로 이동
+				forward = new ActionForward();
+				forward.setRedirect(false);
+				forward.setPath(req.getContextPath()+"/app/admin/user/myInformation.jsp");
+				break;
+			case "/goMyAccount.do":
+				//계정및비밀번호 페이지로 이동
+				forward = new ActionForward();
+				forward.setRedirect(false);
+				forward.setPath(req.getContextPath()+"/app/admin/user/myAccount.jsp");
+				break;
+
+			case "/moveResPage.do":
+				forward = new MoveResPage().execute(req, resp);
+				break;
+
+			case "/acountManager.do":
+				//중간관리자 로그인
+				forward = new ManagerLoginAction().execute(req, resp);
+				break;	
+
 			}
-		
 		
 		
 		
@@ -69,6 +115,8 @@ public class FrontController extends HttpServlet{
 				RequestDispatcher disp = req.getRequestDispatcher(forward.getPath());
 				disp.forward(req, resp);
 			}
+		}else {
+			System.out.println("forward가 null");
 		}
 		
 	}
