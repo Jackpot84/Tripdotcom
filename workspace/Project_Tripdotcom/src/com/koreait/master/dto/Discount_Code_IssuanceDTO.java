b6967d6ab785e1889952c755cc6cb0ed8387a24f
mchee01 <69677873+mchee01@users.noreply.github.com>
2022-02-03 21:51:38 +0900

    2022-02-03 version 0.0.1
    Update master dto,xml,dao 나중에 수정예정.

---
+++ workspace/Project_Tripdotcom/src/com/koreait/master/dto/Discount_Code_IssuanceDTO.java
@@ -0,0 +1,43 @@
+package com.koreait.master.dto;
+
+public class Discount_Code_IssuanceDTO {
+private int Discount_Code_Id;
+private String Deducted_Amount;
+private String Date_Of_Use;
+private String Discount_Code;
+private String Discount_Code_Approval;
+public int getDiscount_Code_Id() {
+	return Discount_Code_Id;
+}
+public void setDiscount_Code_Id(int discount_Code_Id) {
+	Discount_Code_Id = discount_Code_Id;
+}
+public String getDeducted_Amount() {
+	return Deducted_Amount;
+}
+public void setDeducted_Amount(String deducted_Amount) {
+	Deducted_Amount = deducted_Amount;
+}
+public String getDate_Of_Use() {
+	return Date_Of_Use;
+}
+public void setDate_Of_Use(String date_Of_Use) {
+	Date_Of_Use = date_Of_Use;
+}
+public String getDiscount_Code() {
+	return Discount_Code;
+}
+public void setDiscount_Code(String discount_Code) {
+	Discount_Code = discount_Code;
+}
+public String getDiscount_Code_Approval() {
+	return Discount_Code_Approval;
+}
+public void setDiscount_Code_Approval(String discount_Code_Approval) {
+	Discount_Code_Approval = discount_Code_Approval;
+}
+
+
+
+
+}
